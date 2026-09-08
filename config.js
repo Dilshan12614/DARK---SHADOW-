2a74062ea6e75f15aa7f4061fe0c87343825e0ee
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-08 21:15:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  MONGODB: process.env.MONGODB || "mongodb+srv://d1lshanashinsa793_db_user:7IIHK8SYZhbbdAPUm@cluster0.slp4i1y.mongodb.net/DARK_SHADOW?retryWrites=true&w=majority&appName=Cluster0",
   OWNER_NUM: process.env.OWNER_NUM || "94740534738",
 };
